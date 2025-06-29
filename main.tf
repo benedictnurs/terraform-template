989dbfdd72d5ddb6b6e085aac83a02e47643576d
Ben N <93800156+benedictnurs@users.noreply.github.com>
2025-06-29 19:50:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,10 +30,10 @@ terraform {
 variable "tenancy_ocid" { type = string }
 variable "user_ocid" { type = string }
 variable "fingerprint" { type = string }
-variable "private_key_path" { type = string } # PEM file for OCI API
-variable "region" { type = string }        # e.g. "us-ashburn-1"
-variable "compartment_ocid" { type = string } # usually your root compartment OCID
-variable "ssh_public_key" { type = string }   # file("~/.ssh/id_rsa.pub")
+variable "private_key_path" { type = string }
+variable "region" { type = string }
+variable "compartment_ocid" { type = string }
+variable "ssh_public_key" { type = string }
 
 variable "vm_username" {
   type        = string
